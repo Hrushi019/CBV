b1527a4822d60476255ef1fbf3eb29a1222cb844
Hrushikesh <keshwa019@gmail.com>
2020-10-07 14:18:35 +0530

    first commit

---
+++ CBV/urls.py
@@ -0,0 +1,34 @@
+"""CBV URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from myapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+     path('sample/',views.sample,name="sampleFBV"),
+    path('samplecbv/',views.SampleView.as_view(),name="sampleCBV"),
+    path('sample1cbv/',views.Sample1View.as_view(),name="sample1CBV"),
+    path('sample2cbv/',views.Sample2View.as_view(),name="sample2CBV"),
+    path('templatecbv/',views.Template_View.as_view(),name="TemplateCBV"),
+    path('templatecbv1/',views.Template_DemoView.as_view(),name="TemplateCBV1"),
+    path('schools/',views.School_ListView.as_view(),name="school_list"),
+    path('students/',views.Student_ListView.as_view(),name="Student_list"),
+    path('school/<int:pk>',views.School_DetailView.as_view(),name="detail_view"),
+    path('school/create/',views.Create_School.as_view(),name="create_School"),
+    path('school/update/<int:pk>',views.Update_School.as_view(),name="update_school"),
+    path('school/delete/<int:pk>',views.Delete_School.as_view(),name="delete_school"),
+]
---
+++ myapp/views.py
@@ -0,0 +1,78 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from django.views.generic import DeleteView,TemplateView,ListView,UpdateView,DetailView,CreateView
+from myapp.forms import *
+from myapp.models import School,Student
+from django.urls import reverse,reverse_lazy
+#Basic View
+
+# Create your views here.
+
+def sample(request):
+    return HttpResponse("<h1>Function Based Views</h1>")
+
+class SampleView(View):
+    def get(self,request):
+        return HttpResponse("<h1>Class Based Views</h1>")
+
+class Sample1View(View):
+    def get(self,request):
+        return render(request,"sample.html")
+    
+    def post(self,request,*args,**kwargs):
+        data=request.POST.get("name")
+        return HttpResponse("<h1>{}</h1>".format(data))
+
+class Sample2View(View):
+    form_name=SampleForm
+    def get(self,request):
+        form=self.form_name()
+        return render(request,"sample1.html",{'form':form})
+    
+    def post(self,request,*args,**kwargs):
+        form=self.form_name(request.POST)
+        if form.is_valid():
+            return HttpResponse("<h1>{}</h1>".format(form.cleaned_data['name']))
+
+        return render(request,"sample1.html",{'form':form})
+
+class Template_View(TemplateView):
+    template_name="sample.html"
+
+
+class Template_DemoView(TemplateView):
+    template_name="sample2.html"
+
+class School_ListView(ListView):
+    model=School
+    template_name="school_list.html"
+    context_object_name="schools"
+
+class Student_ListView(ListView):
+    model=Student
+    template_name="student_list.html"
+
+class School_DetailView(DetailView):
+    model=School
+    template_name='school_detail.html'
+    context_object_name="school_detail"
+
+    def get_absolute_url(self):
+        return reverse("detail_view", kwargs={"pk": self.pk})
+
+
+class Create_School(CreateView):
+    model=School
+    template_name="school_form.html"
+    fields=('name','principal','location')
+
+class Update_School(UpdateView):
+    model=School
+    template_name="school_form.html"
+    fields=('name','principal','location')
+
+class Delete_School(DeleteView):
+    model=School
+    template_name="delete_school.html"
+    success_url=reverse_lazy('school_list')
